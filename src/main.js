e6a956c494aabda72cc755c008967917f78e3d39
ForYou520 <1207484695@qq.com>
2022-08-06 21:53:01 +0800

    登录功能

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
 
-Vue.config.productionTip = false
+//导入图标库
+import "./assets/fonts/iconfont.css";
+
+// 导入全局样式表
+import "./assets/css/global.css";
+
+import axios from "axios";
+Vue.prototype.$http = axios;
+
+axios.defaults.baseURL = "https://lianghj.top:8888/api/private/v1/";
+
+Vue.config.productionTip = false;
+
+Vue.use(ElementUI);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+  router,
+}).$mount("#app");
